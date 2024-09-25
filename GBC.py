b459c0a3366297a2401a8295eaa1b181068e93c6
Taras Redchuk <58739235+redchuk@users.noreply.github.com>
2024-09-25 12:18:45 +0300

    initial commit

---
+++ GBC.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+import seaborn as sns
+from matplotlib import pyplot as plt
+import shap
+from feature_engineering import get_data  
+
+path = 'tracking_output/data_47091baa.csv'
+outpath = 'shap_averaged_GBC.csv'
+data_from_csv = pd.read_csv(path)
+X, y, indexed = get_data(data_from_csv)
+
+verbose = True
+grid_iterations = 2
+cv_iterations = 2
+sgkf = StratifiedGroupKFold(n_splits=4, shuffle=True)
+groups = indexed['file']
+idx_file_particle = indexed[['file', 'particle']]
+param_grid = {'learning_rate': [0.0001, 0.001, 0.01, 0.1, 1, 10],
+              'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30]}
+
+
+def sh_plot(shap_values, feature_values, feature_names):
+    shap.summary_plot(shap_values,
+                      feature_values,
+                      feature_names=feature_names,
+                      sort=False,
+                      color_bar=False,
+                      plot_size=(10, 10),
+                      )
+
+
+''' GBC grid search '''
+
+pivots = []
+baselines = []
+for i in range(grid_iterations):
+    gbc = GradientBoostingClassifier(n_estimators=1000)
+    grid_search = GridSearchCV(gbc, param_grid, cv=sgkf, refit=False)
+
+    grid_search.fit(X, y, groups=groups)
+
+    pivots.append(pd.pivot_table(pd.DataFrame(grid_search.cv_results_),
+                                 values='mean_test_score',
+                                 index='param_learning_rate',
+                                 columns='param_max_depth'))
+
+    baseline = DecisionTreeClassifier(max_depth=1)
+    baselines.append(np.mean(cross_val_score(baseline, X, y, cv=sgkf, groups=groups)))
+
+mean_accuracy = pd.concat(pivots).mean(level=0)
+sns.heatmap(mean_accuracy, annot=True)
+plt.show()
+plt.close()
+
+print('Baseline performance: ' + str(np.mean(baselines)))
+
+best_max_depth = mean_accuracy.max().idxmax()
+best_learning_rate = mean_accuracy.max(axis=1).idxmax()
+
+
+''' GBC SHAP explanation '''
+
+shap_repeats = pd.DataFrame()
+shap_averaged = pd.DataFrame()
+
+for i in range(cv_iterations):
+    shap_splits = []
+    X_test_splits = []
+    idx_splits = []
+
+    for train_idxs, test_idxs in sgkf.split(X, y, groups):
+        X_train = X.loc[train_idxs]
+        X_test = X.loc[test_idxs]
+        y_train = y.loc[train_idxs]
+        y_test = y.loc[test_idxs]
+
+        gbc = GradientBoostingClassifier(n_estimators=1000, learning_rate=best_learning_rate, max_depth=best_max_depth)
+        gbc.fit(X_train, y_train)
+
+        explainer = shap.TreeExplainer(gbc)
+        shap_values = explainer.shap_values(X_test)
+
+        shap_splits.append(shap_values)
+        X_test_splits.append(X_test)
+        idx_splits.append(idx_file_particle.loc[test_idxs])
+
+        if verbose:
+            plt.title('Single split')
+            sh_plot(shap_values, X_test, X.columns)
+
+    all_X_test_splits = pd.concat(X_test_splits)
+    all_shap_splits = np.concatenate(shap_splits)
+    all_shap_splits_df = pd.DataFrame(all_shap_splits, columns=all_X_test_splits.columns).add_prefix('shap_')
+    all_idx_splits = pd.concat(idx_splits)
+
+    if verbose:
+        plt.title('Aggregated from 4CV splits')
+        sh_plot(all_shap_splits, all_X_test_splits, X.columns)
+
+    list_to_concat = [all_X_test_splits.reset_index(),
+                      all_shap_splits_df.reset_index(),
+                      all_idx_splits.reset_index()]
+
+    shaps_and_features = pd.concat(list_to_concat, axis=1) \
+        .drop('index', axis=1).set_index(['file', 'particle']).add_prefix(str(i) + 'r_')
+
+    shap_repeats = shap_repeats.join(shaps_and_features) if not shap_repeats.empty else shaps_and_features
+
+for col in shaps_and_features.columns:
+    shap_averaged[col] = shap_repeats[[x for x in shap_repeats.columns if col[1:] == x[1:]]].mean(axis=1)
+
+shap_averaged.to_csv(outpath, index=False)
+plt.title('Aggregated from ' + str(cv_iterations) + ' CV repeats')
+sh_plot(shap_averaged.iloc[:, 20:].to_numpy(), shap_averaged.iloc[:, :20].to_numpy(), X.columns)
---
+++ feature_engineering.py
@@ -0,0 +1,109 @@
+import numpy as np
+from scipy.stats import linregress
+
+
+def get_data(data):
+    data['dX'] = data.groupby(['file', 'particle'])['x_micron'].diff()
+    data['dY'] = data.groupby(['file', 'particle'])['y_micron'].diff()
+
+    data_agg = data.groupby(['file', 'particle']).agg(serum=('serum', 'first'),
+
+                                                      # Mean locus displacement
+                                                      MD=('D', 'mean'),
+
+                                                      # Maximal locus displacement
+                                                      MaxD=('D', 'max'),
+
+                                                      temp_sum_D=('D', 'sum'),
+
+                                                      # Variance of locus displacement
+                                                      VarD=('D', 'var'),
+
+                                                      temp_sum_dX=('dX', 'sum'),
+                                                      temp_sum_dY=('dY', 'sum'),
+
+                                                      # Mean nuclear area
+                                                      MA=('A', 'mean'),
+
+                                                      # Mean nuclear perimeter
+                                                      MP=('P', 'mean'),
+
+                                                      # Mean locus distance to nuclear periphery
+                                                      MDist=('Dist', 'mean'),
+
+                                                      temp_min_Dist=('Dist', 'min'),
+                                                      temp_max_Dist=('Dist', 'max'),
+                                                      temp_beg_Dist=('Dist', 'first'),
+                                                      temp_end_Dist=('Dist', 'last'),
+
+                                                      # Variance of locus distance to nuclear periphery
+                                                      VarDist=('Dist', 'var'),
+                                                      )
+
+    # Relative variance of locus displacement
+    data_agg['rVarD'] = data_agg['VarD'] / data_agg['MD']
+
+    # Relative variance of locus distance to nuclear periphery
+    data_agg['rVarDist'] = data_agg['VarDist'] / data_agg['MDist']
+
+    # Total locus displacement
+    data_agg['TD'] = np.sqrt((data_agg['temp_sum_dX']) ** 2 + (data_agg['temp_sum_dY']) ** 2)
+
+    # Persistence of locus movement
+    data_agg['Pers'] = data_agg['TD'] / data_agg['temp_sum_D']
+
+    # True for the fastest homologous locus in the nucleus (highest MD)
+    data_agg['temp_file_MD'] = data_agg.groupby('file')['MD'].transform(np.max)
+    data_agg['ifFast'] = np.where((data_agg['MD'] == data_agg['temp_file_MD']), 1, 0)
+
+    # Range of locus distance to nuclear periphery
+    data_agg['DistR'] = data_agg['temp_max_Dist'] - data_agg['temp_min_Dist']
+
+    # Total locus radial displacement
+    data_agg['TDist'] = data_agg['temp_end_Dist'] - data_agg['temp_beg_Dist']
+
+    # True for the homologous locus furthest from nuclear rim (highest MDist)
+    data_agg['temp_file_max_Dist'] = data_agg.groupby('file')['MDist'].transform(np.max)
+    data_agg['ifCentr'] = np.where((data_agg['MDist'] == data_agg['temp_file_max_Dist']), 1, 0)
+
+    # Trend of change of locus distance to nuclear periphery
+    data_slope = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['Dist'])[0])
+    data_agg['sDist'] = data_slope
+
+    # Trend of change of nuclear area
+    data_sA = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['A'])[0])
+    data_agg['sA'] = data_sA
+
+    # Trend of change of nuclear perimeter
+    data_sP = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['P'])[0])
+    data_agg['sP'] = data_sP
+
+    # Number of displacements 2SD longer than MD
+    data_SD_D = data.groupby(['file', 'particle']).agg(SD_diff=('D', 'std'))
+    data_i = data.set_index(['file', 'particle'])
+    data_i['SD_D'] = data_SD_D
+    data_i['MD'] = data_agg['MD']
+    data_i['out2sd'] = np.where((data_i['D'] > (data_i['MD'] + 2 * data_i['SD_D'])), 1, 0)
+    data_i['out3sd'] = np.where((data_i['D'] > (data_i['MD'] + 3 * data_i['SD_D'])), 1, 0)
+    data_agg['out2sd'] = data_i.groupby(['file', 'particle']).agg(out2sd=('out2sd', 'sum'))
+
+    # Number of displacements 3SD longer than MD
+    data_agg['out3sd'] = data_i.groupby(['file', 'particle']).agg(out3sd=('out3sd', 'sum'))
+
+
+    data = data_agg.drop(['temp_sum_dX',
+                          'temp_sum_dY',
+                          'temp_min_Dist',
+                          'temp_max_Dist',
+                          'temp_beg_Dist',
+                          'temp_end_Dist',
+                          'temp_file_MD',
+                          'temp_file_max_Dist',
+                          'temp_sum_D',
+                          ], axis=1)
+    data.reset_index(inplace=True)
+
+    features = ['MD', 'MaxD', 'VarD', 'MA', 'MP', 'MDist', 'VarDist', 'rVarD', 'rVarDist',
+                'TD', 'Pers', 'ifFast', 'DistR', 'TDist', 'ifCentr', 'sDist', 'sA', 'sP', 'out2sd', 'out3sd']
+
+    return data[features], data['serum'], data
